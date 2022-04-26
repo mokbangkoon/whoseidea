1e1064d55d587b1dc87206ea267b7b71e53274b8
lee chun woo <chunwoolee.work@gmail.com>
2022-04-26 17:20:30 +0900

    [ADD] server 내 게시글 조회 구현

--- server/src/controller/index.ts
+++ server/src/controller/index.ts
@@ -2,12 +2,14 @@ import { signup } from './users/signup'
 import { signout } from './users/signout'
 import { login } from './users/login'
 import { logout } from './users/logout'
+import { myPost } from './users/my-post'
 
 const controllers = {
     signup:signup,
     signout:signout,
     login:login,
-    logout:logout
+    logout:logout,
+    myPost:myPost
 }
 
 export default controllers
