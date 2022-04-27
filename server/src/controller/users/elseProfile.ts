29d141d9dd64ad835a4fe0fecb6fc863a8740e86
lee chun woo <chunwoolee.work@gmail.com>
2022-04-27 17:41:25 +0900

    [FIX] server userId => nickname으로 변경

--- server/src/controller/users/elseProfile.ts
+++ server/src/controller/users/elseProfile.ts
@@ -2,12 +2,12 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 export async function elseProfile (req: any, res: any) {
-    if (!req.query.userId) {
+    if (!req.query.nickname) {
         res.status(423).send('undefined user')
     } else {
         const userInfo: any = await prisma.users.findFirst({
             where: {
-                userId: req.query.userId
+                nickname: req.query.nickname
             }
         })
         delete userInfo?.password
