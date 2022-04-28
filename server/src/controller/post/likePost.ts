3bb673802db285e7e39f7044ac7bd1cd6893786d
lee chun woo <chunwoolee.work@gmail.com>
2022-04-28 15:54:11 +0900

    [ADD] server 게시글 like 기능 추가

---
+++ server/src/controller/post/likePost.ts
@@ -0,0 +1,74 @@
+import { PrismaClient } from "@prisma/client";
+import { isAuthorized } from "../tokenFunctions";
+
+export async function likePost (req: any, res: any) {
+
+    // 쿠키가 없거나 맞지 않으면 오류 처리
+    if (!isAuthorized(req)) {
+        return res.status(405).send('Mismatched Cookies')
+    }
+
+    // 닉네임으로 users 테이블에서 id값을 가져온다.
+    const accsessTokenData: any = isAuthorized(req)
+    const prisma = new PrismaClient();
+    const userInfo = await prisma.users.findFirst({
+        where:{
+            nickname:accsessTokenData.nickname
+        }
+    })
+    
+    // 이미 like가 되어 있는지 확인한다.
+    const isLiked=await prisma.likes.findFirst({
+        where:{
+            nickname:userInfo?.id,
+            postId:req.body.postId
+        }
+    })
+
+    // 만약 like가 되어있다면 라이크를 뺀다.
+    if (isLiked) {
+
+        // like 테이블에서 like한 것을 지운다.
+        await prisma.likes.deleteMany({
+            where:{
+                nickname:userInfo?.id,
+                postId:req.body.postId
+            }
+        })
+
+        // posts 테이블에서 like를 하나 뺀다.
+        await prisma.posts.update({
+            where:{
+                id:req.body.postId
+            },
+            data:{
+                likes:{
+                    increment:-1
+                }
+            }
+        })
+        return res.status(200).send('like minus ok')
+    }
+    // 만약 like가 되어있지 않다면 라이크를 추가한다.
+
+    // 가져온 id값과 body에 들어있는 값을 삽입한다.
+    await prisma.likes.create({
+        data: {
+            nickname:userInfo?.id,
+            postId:req.body.postId
+        }
+    })
+
+    // posts 테이블에서 like를 하나 추가한다.
+    await prisma.posts.update({
+        where: {
+            id: req.body.postId
+        },
+        data: {
+            likes: {
+                increment: 1
+            }
+        }
+    })
+    return res.status(200).send('like plus ok')
+}
