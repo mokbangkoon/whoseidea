dd69b3f5c2d30e174e17d2a02699c76fdc2068cc
lee chun woo <chunwoolee.work@gmail.com>
2022-05-02 17:45:41 +0900

    [FIX] server auth return 문 추가

--- server/src/controller/users/auth.ts
+++ server/src/controller/users/auth.ts
@@ -4,16 +4,16 @@ import { isAuthorized } from '../tokenFunctions'
 
 export async function auth (req: any, res: any) {
     if (!req.headers.cookie) {
-        res.status(401).send({ data: null, message: 'not authorized'})
+        return res.status(401).send({ data: null, message: 'not authorized'})
     }
     if(!isAuthorized(req)) {
-        res.status(406).json(false)
+        return res.status(406).json(false)
     } else {
         const accsessTokenData: any = isAuthorized(req)
         const userInfo: any = await prisma.users.findFirst({
             where: {id: accsessTokenData.id}
         })
         delete userInfo['password']
-        res.status(200).send(userInfo)
+        return res.status(200).send(userInfo)
     }
 }
