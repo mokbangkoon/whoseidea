1bda0563c85ed7ee6e91eaa8d4c6f5527d1fa348
mokbangkoon <ckwlstjd92@naver.com>
2022-05-02 10:01:43 +0900

    [ADD] 럥크폐이지 디자인 구현

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -1,13 +1,32 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../modules';
 import Login from '../components/Login';
+import styled from 'styled-components';
+import RankList from '../components/RankList';
+const Title = styled.div`
+  font-weight: bold;
+  font-size: 100px;
+  text-align: center;
+  position: absolute;
+  color: #051225;
+  border-radius: 1rem;
+  left: 25%;
+  font-family: 'Courier New', Courier, monospace;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))
+    drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))
+    drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  border-radius: 10px;
+`;
 
 export default function Rank() {
   const check = useSelector((state: RootState) => state.modal.check);
 
   return (
     <div>
-      <div>여기는 랭킹 페이지입니다.</div>
+      <Title>
+        <div>최고의 아이디어</div>
+      </Title>
+      <RankList />
       {check ? <Login /> : null}
     </div>
   );
