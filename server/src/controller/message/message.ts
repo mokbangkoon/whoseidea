c129d5ce255b2c382a241995213c4c7a5d35f0a2
Mizulatte <rainbow5284@gmail.com>
2022-05-04 16:54:07 +0900

    [FIX]body값 오류 수정

--- server/src/controller/message/message.ts
+++ server/src/controller/message/message.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 import { isAuthorized } from "../tokenFunctions";
 
-export async function messanger (req :any, res: any) {
+export async function message (req :any, res: any) {
     if (!isAuthorized(req)) {
         return res.status(422).send('invaild')
     }
@@ -16,7 +16,7 @@ export async function messanger (req :any, res: any) {
         return res.status(425).send('not user')
     }
     if (!await prisma.users.findFirst({
-        where: {nickname: req.body.userId}
+        where: {nickname: req.body.nickname}
     })) {
         return res.status(426).send('not find user')
     }
