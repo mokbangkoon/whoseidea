458ac9195ede9d4abb5d7309f3b5627528c42382
mokbangkoon <ckwlstjd92@naver.com>
2022-05-23 10:20:51 +0900

    [FIX] 재포크 후 커밋확인

--- client/src/components/FeatureIdea.tsx
+++ client/src/components/FeatureIdea.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 export default function FeatureIdea() {
-  return <div>아이디어 시연기능</div>;
+  return <div>아이디어 시연기능dd</div>;
 }
